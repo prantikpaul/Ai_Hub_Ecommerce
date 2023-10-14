1050c94a3159d186cd0726a1ff6b2b01fcc031f1
yourusername <28738988+prantikpaul@users.noreply.github.com>
2023-10-14 10:20:11 +0600

    Added Email Verfication

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import sign_in,sign_up,forget_pass,sign_out
+from .views import sign_in,sign_up,forget_pass,sign_out,verify_failed,verify_success,verify
 
 urlpatterns = [
   path('Sign_In/',sign_in,name='sign_inpp'),
   path('Sign_Up/',sign_up,name='sign_uppp'),
   path('Forget_pass/',forget_pass,name='forget_passpp'),
   path('sing_out/',sign_out,name='sign_outpp'),
+  path('verify_success/',verify_success,name='verify_successpp'),
+  path('verify_failed/',verify_failed,name='verify_failedpp'),
+  path('verify/<auth_token>/',verify,name='verifypp'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.models import User,auth
 from django.contrib import messages
+# -----------for forget pass -------------
+from django.contrib.auth import update_session_auth_hash
+#---------------for email verify---------------------------
+import uuid
+from django.core.mail import send_mail
+from django.http import JsonResponse
+from .models import Profile
+from django.conf import settings
 
 # Create your views here.
 
@@ -13,10 +21,16 @@ def sign_in(request):
             Pass=request.POST['pass']
             user=auth.authenticate(username=Uname,password=Pass)
             if user:
-                print(user)
-                auth.login(request,user)
-                messages.success(request, "User Logged In")
-                return redirect('indexpp')
+                prof=Profile.objects.get(user=user) #profile er nam & user er nam same
+                if prof.is_verified is True:
+
+                    print(user)
+                    auth.login(request,user)
+                    messages.success(request, "User Logged In")
+                    return redirect('indexpp')
+                else:
+                    messages.warning(request,'Please Verify Your Mail')
+                    return redirect('verify_failedpp')
             else:
                 messages.warning(request,'Invalid Username or password')
         
@@ -53,8 +67,14 @@ def sign_up(request):
                         )
                         pp.set_password(Pass)
                         pp.save()
-                        messages.success(request, "Account Created Successfully")
-                        return redirect('sign_inpp')
+                        
+                        auth_token = str(uuid.uuid4())
+                        pro_obj = Profile.objects.create(user=pp, auth_token=auth_token) #creatig a profile on this user
+                        pro_obj.save()
+                        send_mail_registration(Email, auth_token)
+                        return render(request, 'accounts/verify_success.html')
+                        # messages.success(request, "Account Created Successfully")
+                        # return redirect('sign_inpp')
                     else:
                         messages.warning(request, "Password Must contain atleast 8 letters !!")
                     
@@ -64,12 +84,50 @@ def sign_up(request):
 
     return render(request,'accounts/Sign_up.html')
 
-def forget_pass(request):
 
-    return render(request,'accounts/Forget_pass.html')
 
 def sign_out(request):
     auth.logout(request)
     messages.success(request, "User Logged Out")
     
     return redirect('indexpp')
+
+def forget_pass(request):
+    if request.method=='POST':
+        Email=request.POST['email']
+        Pass=request.POST['pass']
+        if Email:
+            if len(Pass)>7:
+                pp=User.objects.get(email=Email)
+                pp.set_password(Pass)
+                pp.save()
+                update_session_auth_hash(request,pp)
+                messages.success(request,'Password Changed Successflly')
+            else:
+                messages.warning(request,"Password Must contain atleast 8 letters !!")
+        else:
+            messages.warning(request,"Email Didn't matached")
+
+    return render(request,'accounts/Forget_pass.html')
+
+def verify_success(request):
+    
+    return render(request,'verify_success.html')
+
+def verify_failed(request):
+    
+    return render(request,'verify_failed.html')
+
+def send_mail_registration(Email, token):
+    subject = "Account Verification link"
+    message = f'hi click the link for verify http://127.0.0.1:8000/accounts/verify/{token}'
+    email_from = settings.EMAIL_HOST_USER
+    recipient_list = [Email]
+    send_mail(subject, message, email_from, recipient_list)
+
+def verify(request, auth_token):
+    profile_obj = Profile.objects.filter(auth_token=auth_token).first()
+    profile_obj.is_verified = True
+    profile_obj.save()
+    messages.success(request, 'OWWO,your mail is verified')
+    return redirect('sign_inpp')
