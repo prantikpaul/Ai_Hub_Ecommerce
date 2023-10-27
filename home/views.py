4794d9fa45b7ee6ed5efc18dc923cca5b2958b88
yourusername <28738988+prantikpaul@users.noreply.github.com>
2023-10-28 00:55:30 +0600

    Added Trendding ,Laptop etc product in home page

--- home/views.py
+++ home/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
-from products.models import category
+from products.models import category,product
 
 # Create your views here.
 def index(request):
         all_cat= category.objects.all()
+        laptop_deal=product.objects.filter(category="2")
+        tv_prod=product.objects.filter(category='5')
+        smart_phn=product.objects.filter(category='1')
+        trnding_prod=product.objects.filter(trending_prod=True)
+        
 
         return render(request,'home/index.html',locals())
+
