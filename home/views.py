0ed49ce1f79458a0a41818e83bd21145a3d42c68
yourusername <28738988+prantikpaul@users.noreply.github.com>
2023-10-27 21:53:48 +0600

    Added Products to the products page

--- home/views.py
+++ home/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from products.models import category
 
 # Create your views here.
 def index(request):
+        all_cat= category.objects.all()
 
-        return render(request,'home/index.html')
+        return render(request,'home/index.html',locals())
--- products/urls.py
+++ products/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from . views import prod_page
 
 urlpatterns = [
+    path('all_products/<int:id>/',prod_page,name='all_prod'),
 
 ]
