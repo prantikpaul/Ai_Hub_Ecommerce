9346901015b9e43ccdd77d9e0ed073573959e50f
yourusername <28738988+prantikpaul@users.noreply.github.com>
2023-10-29 00:43:47 +0600

    added cart function in models.py

--- products/models.py
+++ products/models.py
@@ -24,4 +24,13 @@ class product(models.Model):
    def __str__(self):
       return self.name
    
+
+class cart(models.Model):
+   
+   user = models.ForeignKey(User,on_delete=models.CASCADE)
+   product = models.ForeignKey(product, on_delete=models.CASCADE)
+   quantity = models.PositiveBigIntegerField(default=1)
+
+   def __str__(self):
+      return self.user.username
    
